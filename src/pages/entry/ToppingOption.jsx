4dc1e000d458e74022d870e9cdf49cba3cbccfd9
eviva2000 <arjomandielaheh@gamin.com>
2024-02-10 17:54:09 +0100

    fixed checkbox

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useOrderDetails } from "../../contexts/OrderDetails";
 
@@ -12,29 +11,14 @@ const ToppingOption = ({ name, imagePath }) => {
   };
   const [isChecked, setIsChecked] = useState(false);
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: "center" }}>
       <img
-        style={{ width: "%75" }}
+        style={{ width: "75%" }}
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
-
-      <Form.Group
-        controlId={`${name}-count`}
-        as={Row}
-        style={{ marginTop: "10px" }}
-      >
-        <Form.Label column xs="6" style={{ textAlign: "right" }}>
-          {name}
-        </Form.Label>
-        <Col xs="5" style={{ textAlign: "left" }}>
-          <Form.Control
-            checked={isChecked}
-            type="checkbox"
-            defaultValue={0}
-            onChange={handleChange}
-          />
-        </Col>
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
       </Form.Group>
     </Col>
   );
